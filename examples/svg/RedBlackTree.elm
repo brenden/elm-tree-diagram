f24317146f03070ab8b6f8838b8de0febce43dba
Brenden Kokoszka <brenden.kokoszka@gmail.com>
2016-09-03 18:48:43 -0700

    Rough sketch of SVG red black tree example

---
+++ examples/svg/RedBlackTree.elm
@@ -0,0 +1,136 @@
+module Main exposing (..)
+
+import Svg exposing (Svg, svg, circle, line, polygon, rect, g, text', text)
+import Svg.Attributes exposing (..)
+import TreeDiagram exposing (drawSvg, node, Tree, defaultTreeLayout)
+
+
+type Color
+  = Red
+  | Black
+
+
+-- Tree to draw
+
+
+redBlackTree =
+  node
+    (Just ( 13, Black ))
+    [ node
+        (Just ( 8, Red ))
+        [ node
+            (Just ( 1, Black ))
+            [ node Nothing []
+            , node
+                (Just ( 6, Red ))
+                [ node Nothing []
+                , node Nothing []
+                ]
+            ]
+        , node
+            (Just ( 11, Black ))
+            [ node Nothing []
+            , node Nothing []
+            ]
+        ]
+    , node
+        (Just ( 17, Red ))
+        [ node
+            (Just ( 15, Black ))
+            [ node Nothing []
+            , node Nothing []
+            ]
+        , node
+            (Just ( 25, Black ))
+            [ node
+                (Just ( 22, Red ))
+                [ node Nothing []
+                , node Nothing []
+                ]
+            , node
+                (Just ( 27, Red ))
+                [ node Nothing []
+                , node Nothing []
+                ]
+            ]
+        ]
+    ]
+
+(=>) prop value =      
+  prop (toString value)
+
+{-| Represent edges as arrows from parent to child
+-}
+drawEdge : ( Float, Float ) -> Svg msg
+drawEdge ( x, y ) =
+  let
+    arrowOffset =
+      42
+
+    theta =
+      atan (y / x)
+
+    rot' =
+      if x > 0 then
+        theta
+      else
+        pi + theta
+
+    rot =
+      (rot' / (2 * pi)) * 360
+
+    dist =
+      sqrt (x ^ 2 + y ^ 2)
+
+    scale =
+      (dist - arrowOffset) / dist
+
+    (xTo, yTo) =
+      ( scale * x, scale * y )
+  in
+    g
+      []
+      [ line
+          [ x1 => 0, y1 => 0, x2 => xTo, y2 => yTo, stroke "black"]
+          []
+      , g
+          [ transform <| "translate(" ++ (toString xTo) ++ " " ++ (toString yTo) ++ ") " ++
+            "rotate(" ++ (toString rot) ++ ")"]
+          [ arrow ]
+      ]
+
+
+{-| Represent nodes as colored circles with the node value inside.
+-}
+drawNode : Maybe ( Int, Color ) -> Svg msg
+drawNode n =
+  case n of
+    Just ( n, c ) ->
+      let
+        color =
+          case c of
+            Red ->
+              "red"
+
+            Black ->
+              "black"
+      in
+        circle [ r "16", stroke "black", fill color, cx "0", cy "0" ] []
+
+    Nothing ->
+      rect [ width "50", height "40", stroke "black" ] []
+
+
+-- Arrow to go on the ends of edges
+
+
+arrow =
+  polygon [points "-10,10 10,0, -10,-10 -5,0"] []
+
+
+main =
+  drawSvg
+    { defaultTreeLayout | padding = 60, siblingDistance = 80 }
+    drawNode
+    drawEdge
+    redBlackTree
